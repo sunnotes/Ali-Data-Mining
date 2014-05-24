19b758aeb7116abe4248e8420b20ba3fbb46764f
sunnotes <liliangjun88@gmail.com>
2014-05-24 22:33:41 +0800

    upload R file

--- data_analysis.R
+++ data_analysis.R
@@ -43,17 +43,57 @@ barplot(totDate,main='Records per Date',names.arg='',xlab='Dates',ylab='Amount')
 any(is.na(data))
 
 
-#日期
 
-range(as.Date(data$visit_datetime))
 
 
 #install.packages("ggplot2")
 #install.packages("reshape2")
 library(reshape2)
 
-#data2 <- acast(data, user_id ~ brand_id~visit_month~type, length) 
-#head(data2)
+data2 <- acast(data, user_id + brand_id~type, length) 
+head(data2,3)
+tail(data2,3)
 
 
-data[which(data$user_id == 19500),]
+
+
+table(data$type)
+table(data$type,data$visit_month)
+
+#日期
+
+range(as.Date(data$visit_datetime))
+
+####################################################
+#4-6月有行为的用户/7月购买的用户
+
+#7月购买记录数 1365    2053总记录
+records1 <- subset(data,type == 1  & visit_month == "July",select=c(1,2))
+head(records1)
+dim(records1)
+dim(unique(records1))
+
+
+#4-6 活跃数  18511    34907总记录
+records2 <- subset(data,visit_month == c( "April","June" ,"May"),select=c(1,2))
+head(records2)
+dim(records2)
+dim(unique(records2))
+
+#合集 19696         36961总记录
+records3 <- rbind(records1,records2)
+dim(records3)
+dim(unique(records3))
+
+#交集 180
+1365+18511-19696
+
+180/1365
+
+
+temp <- data[which(data$user_id == 71250),]
+table(temp$type,temp$visit_month,temp$brand_id)
+
+length(unique(temp$brand_id))
+temp <- temp[which(temp$type == 1),]
+temp
---
+++ user_brand_analysis.R
@@ -0,0 +1,98 @@
+#switch workspace
+oldwd <- getwd();
+setwd("/mnt/hgfs/WorkSpaces/Ali-Data-Mining")
+
+###########windows
+#install.packages("RODBC")
+#library(RODBC)
+#conn <- odbcConnect('alidata')
+
+########Linux
+#install.packages("RMySQL")
+#library(RMySQL)
+con <- dbConnect(MySQL(),
+                 user='root', 
+                 password='root', 
+                 dbname='ali', 
+                 host="127.0.0.1") 
+#列出所有数据库中的表
+dbListTables(con) 
+
+#活跃数
+active <- dbGetQuery(con,'SELECT user_id,brand_id,
+ COUNT(1)AS active_num 
+ FROM t_alibaba_data 
+ GROUP BY user_id, brand_id  ')
+dim(active)
+head(active)
+
+#月数
+month <- dbGetQuery(con,' SELECT user_id,
+ brand_id,
+ COUNT(DISTINCT(MONTH(visit_datetime)))AS month_num 
+                    FROM t_alibaba_data 
+                    GROUP BY user_id,brand_id  ')
+dim(month)
+head(month)
+
+
+#点击数
+click <- dbGetQuery(con,'SELECT user_id,brand_id,
+                    COUNT(1)AS click_num 
+                    FROM t_alibaba_data 
+                    WHERE TYPE =0
+                    GROUP BY user_id,brand_id  ')
+dim(click)
+head(click)
+
+#购买
+buy <- dbGetQuery(con,'SELECT user_id,brand_id,
+                    COUNT(1)AS buy_num 
+                    FROM t_alibaba_data 
+                    WHERE TYPE =1
+                    GROUP BY user_id,brand_id  ')
+dim(buy)
+head(buy)
+
+
+#收藏
+collect <- dbGetQuery(con,'SELECT user_id,brand_id,
+                    COUNT(1)AS collect_num 
+                    FROM t_alibaba_data 
+                    WHERE TYPE =2
+                    GROUP BY user_id,brand_id  ')
+dim(click)
+head(click)
+
+#购物车
+cart <- dbGetQuery(con,'SELECT user_id,brand_id,
+                    COUNT(1)AS cart_num 
+                    FROM t_alibaba_data 
+                    WHERE TYPE =3
+                    GROUP BY user_id,brand_id  ')
+dim(cart)
+head(cart)
+
+#合并
+
+user_brand <- merge(active,merge(month,merge(click,merge(collect,merge(cart,buy,all=TRUE),all=TRUE),all=TRUE),all=TRUE),all=TRUE)
+
+dim(user_brand)
+head(user_brand)
+
+user_brand$click_buy_ratio <-  user_brand$buy_num/user_brand$click_num
+user_brand$collect_buy_ratio <-  user_brand$buy_num/user_brand$collect_num
+user_brand$cart_buy_ratio <-  user_brand$buy_num/user_brand$cart_num
+
+
+user_brand[is.na(user_brand)] = 0
+
+user_brand <- user_brand[-c(5,6,7)]
+
+dim(user_brand)
+head(user_brand)
+
+
+save(user_brand,file ='data/user_brand.RData')
+
+write.csv(user_brand,file="data/user_brand.csv")
