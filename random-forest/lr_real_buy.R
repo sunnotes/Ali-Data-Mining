804fda086b7acc43f001440ef2f9f52dd7baa907
root <liliangjun88@gmail.com>
2014-06-01 18:13:58 +0800

    add rf

---
+++ random-forest/lr_real_buy.R
@@ -0,0 +1,40 @@
+#switch workspace
+oldwd <- getwd();
+setwd("/mnt/hgfs/WorkSpaces/Ali-Data-Mining")
+
+###########windows
+#install.packages("RODBC")
+#library(RODBC)
+#conn <- odbcConnect('alidata')
+
+########Linux
+#install.packages("DBI")
+#install.packages("RMySQL")
+library(RMySQL)
+con <- dbConnect(MySQL(),
+                 user='root', 
+                 password='root', 
+                 dbname='ali', 
+                 host="127.0.0.1") 
+#列出所有数据库中的表
+dbListTables(con) 
+
+#查询数据库 train
+real_buy <- dbGetQuery(con,'SELECT 
+user_id,
+brand_id,
+                                  COUNT(1) AS buy_times
+                                  FROM t_alibaba_data 
+                                  WHERE visit_datetime > "2014-07-15"
+                                  AND TYPE = 1
+                                  GROUP BY user_id,brand_id
+')
+dim(real_buy)
+head(real_buy)
+
+real_buy$real_buy = 1
+real_buy<- real_buy[,-3]
+head(real_buy)
+
+
+
